e5bf2e3370ec0d9ad065835028bc623b74a6279a
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-05-28 13:03:02 +0200

    Switch to grpc

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	pb "github.com/graphql-services/oauth/grpc"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (s *server) Validate(ctx context.Context, req *pb.ValidateRequest) (*pb.ValidateResponse, error) {
+	return &pb.ValidateResponse{Valid: true, Scopes: req.Scopes}, nil
+}
+
+const defaultPort = "80"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterScopeValidatorServer(s, &server{})
+
+	fmt.Println("Listening on", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
